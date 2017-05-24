4224c033a458c6db0064b39ff30c62ab5041778f
Noturno <Noturno@192.168.7.21>
2017-05-23 21:33:41 -0300

    exe

---
+++ Scanner/src/exercicio/numero15/Classe.java
@@ -0,0 +1,22 @@
+/*
+ * Vamos considerar que temos que validar um campo texto de uma tela de cadastro
+de pessoa, no qual é um campo de observação. Caso a quantidade de caracteres
+que o usuário digitou for maior que 50, imprima false, caso contrário imprima true.
+ */
+package exercicio.numero15;
+
+import java.util.Scanner;
+
+public class Classe {
+public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	
+	 String texto;
+	 
+	 System.out.println("Digite seu cadastro");
+	 texto = scanner.nextLine();
+	 
+	 if()
+	 
+}
+}
